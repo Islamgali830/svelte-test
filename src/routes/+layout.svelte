bca00a0a1be2561c0531edfde568277ffe0cb4dd
Islamgali <islamgalizaripov@gmail.com>
2024-02-28 11:53:42 +0500

    add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,11 @@
         <div class="nav-links">
             
             <!-- svelte-ignore missing-declaration -->
-            {#if link.isShow}
+            {#each nav as link}
 
-<a href={link.href} class="link">{link.title}</a>
-
-{/if}
+            <a href={link.href} class="link">{link.title}</a>
+            
+            {/each}
         </div>
     </div>
 </nav>
