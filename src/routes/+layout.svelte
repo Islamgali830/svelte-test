209f12ec8ab30a632454fe6879ad64bdb4740bb0
Islamgali <islamgalizaripov@gmail.com>
2024-02-28 10:55:41 +0500

    +layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
     <div class ="nav-container">
         <a href="/" class="nav-logo" title="Back to Homepage">Исламгали Зарипов</a>
         <div class="nav-links">
-            <a href="/about" class="link">Обо мне</a>
-            <a href="/blog" class="link">Блог</a>
-            <a href="/projects" class="link">Портфолио</a>
-            <a href="/contact" class="link">Контакты</a>
+            <!-- svelte-ignore missing-declaration -->
+            {#each nav as link}
+
+            <a href={link.href} class="link">{link.title}</a>
+            
+            {/each}
         </div>
     </div>
 </nav>
@@ -15,9 +17,35 @@
         <slot></slot>
 </div>
 
+<script>
+    const nav = [
+{
+    title: 'Обо мне',
+    href:'/about'
+},
+{
+    title: 'Блог',
+    href:'/blog'
+},
+{
+    title: 'Портфолио',
+    href:'/project'
+},
+{
+    title: 'Контакты',
+    href:'/contact'
+},
+{
+    title: 'temp',
+    href: '/temp'
+}
+    ]
+    </script>
+
 <style>
     .container{
         max-width: 1400px;
         margin: 50px auto;
     }
 </style>
+
